1c4f5207c7a6e61db4cc241ae424822cc5e535bd
jannec22 <jannec1290@gmail.com>
2020-10-14 11:28:24 +0200

    changed approach

--- find_letter_frequency.py
+++ find_letter_frequency.py
@@ -8,8 +8,7 @@ letter = 'a'
 if len(sys.argv) > 1:
     letter = sys.argv[1]
 
-dict = {'rest':0}
-dict[letter] = 0
+results=[]
 max_records = 1000
 source_index = 1
 
@@ -17,6 +16,8 @@ if len(sys.argv) > 2:
     max_records = int(sys.argv[2])
 
 def print_records(url, source):
+    dict = {'rest':0}
+    dict[letter] = 0
     hit = 1
     resp = requests.get(url, stream=True)
 
@@ -34,7 +35,9 @@ def print_records(url, source):
                                 dict['rest'] +=1
 
                         if dict['rest'] > 0:
-                            print("\rsource(" + str(source) + ") hit(" + str(hit) + ") \"" + letter + "\" frequency: " + "{:.6f}%".format(dict[letter] / dict['rest'] * 100), end="")
+                            subresult = dict[letter] / dict['rest'] * 100
+                            results.append(subresult)
+                            print("\rsource(" + str(source) + ") hit(" + str(hit) + ") \"" + letter + "\" frequency: " + "{:.6f}%".format(subresult), end="")
                             hit += 1
             else:
                 print("\r----------source record limit reached.----------")
@@ -50,4 +53,7 @@ except KeyboardInterrupt:
     print('\nInterrupted')
     sys.exit(0)
 
-print("final \"" + letter + "\" frequency: " + "{:.6f}%".format(dict[letter] / dict['rest'] * 100))
+if len(results) > 0:
+    print("final \"" + letter + "\" frequency: " + "{:.6f}%".format(sum(results)/len(results)))
+else:
+    print("no results mesured")
