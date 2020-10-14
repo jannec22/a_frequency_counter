f00c69f5d4050de88f2f53ef6813b2d996d99040
jannec22 <jannec1290@gmail.com>
2020-10-14 12:58:03 +0200

    videos and fixed generator

--- get_random_internet_portion.sh
+++ get_random_internet_portion.sh
@@ -8,17 +8,19 @@ help(){
 
 [ "$1" = "-h" ] || [ "$1" = "--help" ] && help && exit 0
 
-[ -n "$2" ] && [ "$2" -gt 2020 ] && echo "year can be max 2020" && exit 1
-[ -n "$2" ] && [ "$2" -lt 2007 ] && echo "year have to be at least 2007" && exit 1
+[ -n "$2" ] && ! cat segments.csv | cut -d ',' -f1 | grep -q >/dev/null >&2 "$2" && echo "year have to be in a list of segments (check segments.csv)">&2 && exit 1
+[ -n "$3" ] && ! cat segments.csv | cut -d ',' -f2 | grep -q >/dev/null >&2 "$3" && echo "day have to be in a list of segments (check segments.csv)">&2 && exit 1
 
-RAND_SEG="$(shuf -n 1 < segments.csv)"
-Y="$(echo "$RAND_SEG" | cut -d ',' -f1)"
-DD="$(echo "$RAND_SEG" | cut -d ',' -f2)"
 MAX_PATHS="10"
 
 [ -n "$1" ] && MAX_PATHS="$1"
 [ -n "$2" ] && Y="$2"
 [ -n "$3" ] && DD="$3"
+
+RAND_SEG="$(shuf -n 1 < segments.csv)"
+[ -z "$Y" ] && Y="$(echo "$RAND_SEG" | cut -d ',' -f1)"
+[ -z "$Y" ] && DD="$(echo "$RAND_SEG" | cut -d ',' -f2)"
+[ -z "$DD" ] && DD="$(cat segments.csv | cut -d ',' -f1,2 | grep "$Y," | cut -d ',' -f2 | shuf -n 1)"
 
 curl -fs "https://commoncrawl.s3.amazonaws.com/crawl-data/CC-MAIN-$Y-$DD/warc.paths.gz" | gzip -d | shuf -n "$MAX_PATHS" | while read url; do
     echo "https://commoncrawl.s3.amazonaws.com/$url"
